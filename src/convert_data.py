de4f30c8b272b3c53fa46fb06bb839faaddf378c
William Xie <wxie@cs.utexas.edu>
2015-11-15 02:25:44 -0600

    I think it's working for the entire actitracker set

--- src/convert_data.py
+++ src/convert_data.py
@@ -5,18 +5,47 @@ import h5py
 import csv
 import sys
 
-data_path = "/home/users/wxie/activity/data/actitracker_test_2.txt"
+file_dir = os.path.dirname(os.path.abspath(__file__))
+print(file_dir)
+data_path = "/home/users/wxie/activity/data/actitracker_2.txt"
 
 # Caffe blobs have the dimensions (n_samples, n_channels, height, width)
 # Count lines
-num_lines = sum(1 for line in open(data_path, 'rt'))
-print("Number of data points: {}".format(num_lines))
-
-height = 1
-width = 2
-num_samples = num_lines / width
+num_lines = 0
+num_samples = 0
 num_channels = 1
+height = 1
+width = 64
+# num_samples = num_lines / width
+
+with open(data_path, 'rt') as f:
+    reader = csv.reader(f)
+    prev_label = " "
+    line_count = 0
+    data_index = 0
+    x_list = []
+    for row in reader:
+        line_count += 1
+        label = row[1]
+        # Reset when new label is found
+        if (prev_label != label):
+            prev_label = label
+            x_list = []
+        x_list.append(1)
+
+        # Store when we have the length
+        if (len(x_list) >= width):
+            data_index += 1
+            x_list = []
+    num_samples = data_index
+    num_lines = line_count
+
+print("Number of data points: {}".format(num_lines))
+print("Number of data segments: {}".format(num_samples))
+
 total_size = num_samples * num_channels * height * width
+
+
 
 data = np.arange(total_size)
 data = data.reshape(num_samples, num_channels, height, width)
@@ -25,27 +54,26 @@ data = data.astype('float32')
 data_label = 1 + np.arange(num_samples)[:, np.newaxis]
 data_label = data_label.astype('int32')
 
-# print(label[1][0])
-
-# print(label)
-
-# print(data)
-
 with open(data_path, 'rt') as f:
     reader = csv.reader(f)
     prev_label = " "
     data_index = 0
     channel_index = 0
+    counter = 0
     x_list = []
     y_list = []
     z_list = []
     for row in reader:
+        if not row:
+            continue
+        # print(counter)
+        counter += 1
         user_id = int(row[0])
         label = row[1]
         x = float(row[3])
         y = float(row[4])
         z = float(row[5])
-        print("user_id: {}\t label: {}\t x: {}\t y: {}\t z: {}".format(user_id, label, x, y, z))
+        # print("user_id: {}\t label: {}\t x: {}\t y: {}\t z: {}".format(user_id, label, x, y, z))
 
         # Reset when new label is found
         if (prev_label != label):
@@ -60,6 +88,10 @@ with open(data_path, 'rt') as f:
             data_label[data_index] = np.array([hash(label)])
             data_index += 1
             x_list = [] ###
+    print(data_index)
 
-print(data)
-print(data_label)
+with h5py.File(file_dir + '/../sample_data.h5', 'w') as f:
+    f['data'] = data
+    f['label'] = data_label
+
+print("Done.")
